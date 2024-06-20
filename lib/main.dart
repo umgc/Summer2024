6a230d1453ae9666e1068db7350abd1bbd1ce5b2
Brian Walters <brian2af@outlook.com>
2024-06-19 19:34:07 -0500

    Added Text to Main Dart File.

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times:  This is a test You should see this change. ',
             ),
             Text(
               '$_counter',
