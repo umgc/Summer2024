af01d48696ba40d58aee6c49f1e2acf276552159
Stephen Buley <stephen.a.buley@gmail.com>
2024-06-26 14:33:30 +0200

    remove comments and clean up

--- Team-C/test_wizard/lib/services/llm_service.dart
+++ Team-C/test_wizard/lib/services/llm_service.dart
@@ -2,25 +2,21 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class LLMService {
-  String
-      url; // would this come from a provider? As in llm = new LLMProvider(), then llm.url, llm.apiKey, etc.
+  String url = 'https://api.perplexity.ai/chat/completions';
   String apiKey = const String.fromEnvironment('API_KEY');
+  http.Response? response;
 
-  LLMService({
-    this.url = 'https://api.perplexity.ai/chat/completions',
-  }); // or this.llm = new LLMProvider?
+  LLMService();
 
   Future<http.Response> sendRequest(String prompt) {
     return http.post(
       Uri.parse(url),
       headers: <String, String>{
-        // or llm.generateHeaders()?
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $apiKey'
       },
       body: jsonEncode({
-        // or llm.generateBody()?
         'model': 'llama-3-sonar-small-32k-online',
         'messages': [
           {
