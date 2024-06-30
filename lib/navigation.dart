6ea250cc165a25b5361ee665e4fed41bce892769
Brian Walters <brian2af@outlook.com>
2024-06-29 19:10:57 -0500

    App Drawer Functionality Implemented

---
+++ lib/navigation.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Widget build(BuildContext context)
+{
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('MindinSYNC'),
+    )
+    );
+}
+
+
+
+
+
+
+  
+
