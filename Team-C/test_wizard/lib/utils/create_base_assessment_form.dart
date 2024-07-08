727eea9290fda5f044b7809b6ca5587cdfad9641
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-08 11:19:48 +0200

    [TeamC] start of base assessement form, assessment name and course select

---
+++ Team-C/test_wizard/lib/utils/create_base_assessment_form.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:test_wizard/utils/validators.dart';
+import 'package:test_wizard/widgets/course_select.dart';
+
+class CreateBaseAssessmentForm extends StatefulWidget {
+  const CreateBaseAssessmentForm({super.key});
+
+  @override
+  State<CreateBaseAssessmentForm> createState() => BaseAssessmentFormState();
+}
+
+class BaseAssessmentFormState extends State<CreateBaseAssessmentForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final GlobalKey _formKey = GlobalKey<FormState>();
+  final TextEditingController assessmentController = TextEditingController();
+  final String courseName = 'Select Course';
+  final TextEditingController numOfStudentsController = TextEditingController();
+  final TextEditingController subjectDescriptionController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(label: Text('Assessment Name')),
+            controller: assessmentController,
+            validator: Validators.checkIsEmpty,
+          ),
+          const CourseSelect(),
+        ],
+      ),
+    );
+  }
+}
