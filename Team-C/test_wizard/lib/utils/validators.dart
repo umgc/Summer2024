aba81c691b4d27df10e620c1c29ab5051da21c0d
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-09 13:41:09 +0200

    [TeamC] add one or two digits validator

--- Team-C/test_wizard/lib/utils/validators.dart
+++ Team-C/test_wizard/lib/utils/validators.dart
@@ -8,8 +8,15 @@ class Validators {
 
   static String? checkCourseHasBeenSelected(String? value) {
     if (value == 'Select Course') {
-      return 'Please select a course';
+      return 'Please select a course.';
     }
     return null;
+  }
+
+  static String? checkIsOneOrTwoDigits(String? value) {
+    if (value?.length == 1 || value?.length == 2) {
+      return null;
+    }
+    return 'Please enter one or two digits.';
   }
 }
