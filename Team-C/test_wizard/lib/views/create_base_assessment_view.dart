9812da4ae597e80dd00bc6dc770bd11e1687d186
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-09 16:13:20 +0200

    [TeamC] refactor imports

--- Team-C/test_wizard/lib/views/create_base_assessment_view.dart
+++ Team-C/test_wizard/lib/views/create_base_assessment_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test_wizard/utils/create_base_assessment_form.dart';
-import 'package:test_wizard/utils/customized_widgets.dart';
+import 'package:test_wizard/widgets/create_base_assessment_form.dart';
+import 'package:test_wizard/widgets/customized_widgets.dart';
 
 class CreateBaseAssessmentView extends StatelessWidget {
   const CreateBaseAssessmentView({super.key});
