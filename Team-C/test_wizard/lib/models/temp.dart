308779fec0a60156e9955ea83c7664aed62d5990
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-09 14:01:03 +0200

    [TeamC] add new models for dropdown options

--- Team-C/test_wizard/lib/models/temp.dart
+++ Team-C/test_wizard/lib/models/temp.dart
@@ -1,5 +1,23 @@
 class TempModel {
-  static Future<List<String>> fetchDropdownOptions() async {
-    return ['Select Course', 'Course 1', 'Course 2'];
+  static Future<List<String>> fetchDropdownOptions(String optionName) async {
+    switch (optionName) {
+      case 'Course':
+        return ['Select Course', 'Course 1', 'Course 2'];
+      case 'Assessment Type':
+        return [
+          'Select Assessment Type',
+          'Short Answer',
+          'Multiple Choice',
+          'Essay'
+        ];
+      case 'Graded On':
+        return [
+          'Select Grading Basis',
+          'Standards Based Grading',
+          'Points Based Grading'
+        ];
+      default:
+        return [];
+    }
   }
 }
