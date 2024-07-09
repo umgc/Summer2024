8bbc0020ed8a366dda176895072b405fc41fd215
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-09 18:54:00 +0200

    [TeamC] validators_test complete

--- Team-C/test_wizard/test/utils_tests/validators_test.dart
+++ Team-C/test_wizard/test/utils_tests/validators_test.dart
@@ -11,8 +11,20 @@ void main() {
       expect(Validators.checkIsEmpty("SWEN"), null);
     });
 
-    test('checkOptionHasBeenSelected', () {});
+    test('checkOptionHasBeenSelected', () {
+      String rejectString = 'Please select an option.';
+      expect(Validators.checkOptionHasBeenSelected('Selectthis'), rejectString);
+      expect(Validators.checkOptionHasBeenSelected('Select One of these'),
+          rejectString);
+      expect(Validators.checkOptionHasBeenSelected('Course 1'), null);
+    });
 
-    test('checkIsOneOrTwoDigits', () {});
+    test('checkIsOneOrTwoDigits', () {
+      String rejectString = 'Please enter one or two digits.';
+      expect(Validators.checkIsOneOrTwoDigits(''), rejectString);
+      expect(Validators.checkIsOneOrTwoDigits('12345'), rejectString);
+      expect(Validators.checkIsOneOrTwoDigits('12'), null);
+      expect(Validators.checkIsOneOrTwoDigits('1'), null);
+    });
   });
 }
