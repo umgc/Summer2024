104abb0b0268edf43dc9a830eaa3702aa5619d28
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-13 23:38:54 +0200

    [TeamC] finish tests for teached dashboard

--- Team-C/test_wizard/test/widget_tests/teacher_dashboard_view_test.dart
+++ Team-C/test_wizard/test/widget_tests/teacher_dashboard_view_test.dart
@@ -11,12 +11,54 @@ void main() {
         ),
       );
       expect(find.byType(AppBar), findsNWidgets(2));
+      expect(find.widgetWithText(AppBar, "Teacher's Dashboard"),
+          findsAtLeastNWidgets(1));
       expect(find.byType(ElevatedButton), findsAtLeastNWidgets(2));
       expect(find.widgetWithText(ElevatedButton, 'Create Assessment'),
           findsOneWidget);
       expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
       expect(find.byType(SearchFilter), findsOneWidget);
       expect(find.byType(DataTable), findsOneWidget);
+    });
+
+    testWidgets('Search Filter correctly searches', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: TeacherDashboard(),
+        ),
+      );
+      await tester.pumpAndSettle();
+      // THIS TEST WILL HAVE TO BE CHANGED WHEN WE LOAD REAL DATA
+      expect(
+        find.widgetWithText(ElevatedButton, 'Open'),
+        findsExactly(2),
+        reason:
+            'This test assumes the Assessment Table is getting default data',
+      );
+      await tester.enterText(find.byType(TextField), 'Math');
+      await tester.pumpAndSettle();
+      expect(
+        find.widgetWithText(ElevatedButton, 'Open'),
+        findsExactly(1),
+        reason:
+            'This test assumes the Assessment Table is getting default data',
+      );
+      await tester.enterText(find.byType(TextField), 'Geography');
+      await tester.pumpAndSettle();
+      expect(
+        find.widgetWithText(ElevatedButton, 'Open'),
+        findsNothing,
+        reason:
+            'This test assumes the Assessment Table is getting default data, Math Quiz 1, Science Test',
+      );
+      await tester.enterText(find.byType(TextField), '');
+      await tester.pumpAndSettle();
+      expect(
+        find.widgetWithText(ElevatedButton, 'Open'),
+        findsExactly(2),
+        reason:
+            'This test assumes the Assessment Table is getting default data, Math Quiz 1, Science Test',
+      );
     });
   });
 }
