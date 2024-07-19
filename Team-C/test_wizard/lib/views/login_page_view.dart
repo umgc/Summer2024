db7795d6e8c18b250b1e876343a230f546299921
Stephen Buley <stephen.a.buley@gmail.com>
2024-07-19 13:49:52 +0200

    [TeamC] cleanup create assessment form file and fix UI errors in Login Page

--- Team-C/test_wizard/lib/views/login_page_view.dart
+++ Team-C/test_wizard/lib/views/login_page_view.dart
@@ -6,95 +6,103 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: const Color(0xffe6f2ff),
       body: Center(
-        child: Container(
-          width: 400,
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                color: const Color(0xffff6600),
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'TestWizard',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: screenSize.height - kToolbarHeight * 2,
+            ),
+            child: Container(
+              width: 400,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
                   ),
-                ),
+                ],
               ),
-              Container(
-                color: const Color(0xff0072bb),
-                padding: const EdgeInsets.all(20),
-                child: const Text(
-                  'Login to TestWizard',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Image.asset(
-                'lib/assets/wizard2.png', // Ensure this path is correct
-                width: 200,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Your OAuth login logic here
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      // to Teacher Dashboard with 'logged in' status for now
-                      builder: (context) => const TeacherDashboard(
-                        status: 'logged in',
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    color: const Color(0xffff6600),
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'TestWizard',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
                       ),
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xff0072bb),
-                  foregroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  textStyle: const TextStyle(fontSize: 16),
-                ),
-                child: const Text('Login with Moodle'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  //Guest Login here
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      // to Teacher Dashboard with no info for now
-                      builder: (context) => const TeacherDashboard(),
+                  ),
+                  Container(
+                    color: const Color(0xff0072bb),
+                    padding: const EdgeInsets.all(20),
+                    child: const Text(
+                      'Login to TestWizard',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xffff6600),
-                  foregroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  textStyle: const TextStyle(fontSize: 16),
-                ),
-                child: const Text('Login as Guest'),
+                  ),
+                  const SizedBox(height: 20),
+                  Image.asset(
+                    'lib/assets/wizard2.png', // Ensure this path is correct
+                    width: 200,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Your OAuth login logic here
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          // to Teacher Dashboard with 'logged in' status for now
+                          builder: (context) => const TeacherDashboard(
+                            status: 'logged in',
+                          ),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff0072bb),
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      textStyle: const TextStyle(fontSize: 16),
+                    ),
+                    child: const Text('Login with Moodle'),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      //Guest Login here
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          // to Teacher Dashboard with no info for now
+                          builder: (context) => const TeacherDashboard(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffff6600),
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      textStyle: const TextStyle(fontSize: 16),
+                    ),
+                    child: const Text('Login as Guest'),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- Team-C/test_wizard/lib/widgets/create_base_assessment_form.dart
+++ Team-C/test_wizard/lib/widgets/create_base_assessment_form.dart
@@ -27,137 +27,133 @@ class BaseAssessmentFormState extends State<CreateBaseAssessmentForm> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        Size screenSize = MediaQuery.of(context).size;
-        return Center(
-          child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: screenSize.height - kToolbarHeight * 2,
-              ),
-              child: Container(
-                width: 1200,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                    ),
-                  ],
+    Size screenSize = MediaQuery.of(context).size;
+    return Center(
+      child: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: screenSize.height - kToolbarHeight * 2,
+          ),
+          child: Container(
+            width: 1200,
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
                 ),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: SizedBox(
-                    width: screenSize.width * 0.8,
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'Assessment Name',
-                              border: OutlineInputBorder(),
+              ],
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: screenSize.width * 0.8,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Assessment Name',
+                          border: OutlineInputBorder(),
+                        ),
+                        controller: assessmentController,
+                        validator: Validators.checkIsEmpty,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // ** Select Course Dropdown
+                      DropdownSelect(
+                        controller: courseNameController,
+                        dropdownTitle: 'Course',
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // ** Number of Tests **
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Number of Tests',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        controller: numOfStudentsController,
+                        validator: Validators.checkIsOneOrTwoDigits,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // ** Subject Description **
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Subject Description',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.multiline,
+                        minLines: null,
+                        maxLines: 4,
+                        validator: Validators.checkIsEmpty,
+                        controller: subjectDescriptionController,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF0072BB),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
                             ),
-                            controller: assessmentController,
-                            validator: Validators.checkIsEmpty,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          // ** Select Course Dropdown
-                          DropdownSelect(
-                            controller: courseNameController,
-                            dropdownTitle: 'Course',
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          // ** Number of Tests **
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'Number of Tests',
-                              border: OutlineInputBorder(),
-                            ),
-                            keyboardType: TextInputType.number,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                            controller: numOfStudentsController,
-                            validator: Validators.checkIsOneOrTwoDigits,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          // ** Subject Description **
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: 'Subject Description',
-                              border: OutlineInputBorder(),
-                            ),
-                            keyboardType: TextInputType.multiline,
-                            minLines: null,
-                            maxLines: 4,
-                            validator: Validators.checkIsEmpty,
-                            controller: subjectDescriptionController,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xFF0072BB),
-                                  foregroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                  ),
-                                ),
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    String assessmentName =
-                                        assessmentController.text;
-                                    String course = courseNameController.text;
-                                    String numOfStudents =
-                                        numOfStudentsController.text;
-                                    String subjectDescription =
-                                        subjectDescriptionController.text;
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                String assessmentName =
+                                    assessmentController.text;
+                                String course = courseNameController.text;
+                                String numOfStudents =
+                                    numOfStudentsController.text;
+                                String subjectDescription =
+                                    subjectDescriptionController.text;
 
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Column(
-                                          children: [
-                                            Text(
-                                                'Assessment: $assessmentName, Course: $course, Students: $numOfStudents, Subject: $subjectDescription'),
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  }
-                                },
-                                child: const Text('Generate Assessment'),
-                              ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              const CancelButton(),
-                            ],
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Column(
+                                      children: [
+                                        Text(
+                                            'Assessment: $assessmentName, Course: $course, Students: $numOfStudents, Subject: $subjectDescription'),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }
+                            },
+                            child: const Text('Generate Assessment'),
                           ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          const CancelButton(),
                         ],
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
