efe49ae0e49e118b9ede5d2cc1c469e631b8bcab
Eric Bennett <aribennett1@gmail.com>
2024-07-18 22:04:52 -0400

    Updated controller with stubs

--- TeamB/IntelliGrade/lib/controller/main_controller.dart
+++ TeamB/IntelliGrade/lib/controller/main_controller.dart
@@ -3,25 +3,61 @@ import 'assessment_generator.dart';
 import 'assessment_grader.dart';
 
 class MainController {
-  final AssessmentGenerator assessmentGenerator = AssessmentGenerator(serverUrl: '');//TODO
+  final AssessmentGenerator assessmentGenerator =
+      AssessmentGenerator(serverUrl: ''); //TODO
   final AssessmentGrader assessmentGrader = AssessmentGrader();
 
   void createAssessment() {
+    //will use LLM
     // Assessment assessment = assessmentGenerator.generateAssessment();
     // Handle the creation logic
   }
 
-  void viewAssessment() {
-    // Handle viewing logic
-  }
-
-  void settings() {
-    // Handle settings logic
-  }
-
   void gradeAssessment() {
+    //will use LLM
     // Assessment assessment = assessmentGenerator.generateAssessment();
     // Assessment gradedAssessment = assessmentGrader.gradeAssessment(assessment);
     // Handle grading logic
+  }
+
+  Assessment viewLocalAssessment(String filename) {
+    // Handle viewing logic
+    return Assessment();
+  }
+
+  void settings() {
+    //what is this supposed to do?
+    // Handle settings logic
+  }
+
+  void saveFileLocally(String assessmentAsXml) {
+    // Handle saving logic
+  }
+
+  void downloadAssessmentAsPdf(String filename) {
+    // Handle downloading logic
+  }
+
+  List<String> listAllAssessments() {
+    // Handle listing logic
+    return [];
+  }
+
+  void updateFileLocally(String assessmentAsXml) {
+    // Handle updating file logic
+  }
+
+  void postAssessmentToMoodle() {
+    // Handle posting logic
+  }
+
+  Assessment getAssessmentFromMoodle() {
+    // Handle getting logic
+    return Assessment();
+  }
+
+  String complieCodeAndGetOutput(String code) {
+    // Handle compiling logic
+    return '';
   }
 }
