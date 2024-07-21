28c2330650088b4ed35d0b862887e5f869a523d6
Eric Bennett <aribennett1@gmail.com>
2024-07-21 17:23:47 -0400

    Added local storage CRUD operations to business layer

--- TeamB/IntelliGrade/lib/controller/question.dart
+++
@@ -1,9 +0,0 @@
-enum QuestionType { multipleChoice, trueFalse, shortAnswer }
-
-class Question {
-  String question;
-  QuestionType type;
-  var response;
-
-  Question(this.question, this.type, this.response);
-}
