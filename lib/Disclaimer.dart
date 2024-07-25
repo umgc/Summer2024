aa6a7aee53bb68a7cb559e01d35d22350870d7df
shall129 <shall129@student.umgc.edu>
2024-07-25 02:28:08 -0400

    disclaimer updated

--- lib/Disclaimer.dart
+++ lib/Disclaimer.dart
@@ -16,10 +16,13 @@ class DisclaimerPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Please read and agree to the terms and conditions to proceed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor tincidunt ligula. Maecenas at rutrum sapien. Sed a neque id dui imperdiet accumsan. Nam et vulputate massa. Sed pellentesque pretium sagittis. ',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Text(
+                'Please read and agree to the terms and conditions to proceed. \n \nPlease be aware that biometric login features, including fingerprint and facial recognition, are designed to enhance security and convenience. However, no system is completely infallible, and the accuracy of biometric authentication can be affected by various factors such as environmental conditions, hardware limitations, or changes in your physical characteristics. By using biometric login, you acknowledge that while these methods are intended to provide a higher level of security, they are not immune to errors or potential security breaches. For your safety, it is advisable to use biometric authentication in conjunction with other security measures, such as strong passwords or multi-factor authentication, where applicable.',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 16),
+              ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
