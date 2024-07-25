7292cb90f0720fe27c59e7aa0743297a8d1766b7
shall129 <shall129@student.umgc.edu>
2024-07-25 01:12:18 -0400

    Add ability to view images in document folder

--- lib/document_upload.dart
+++ lib/document_upload.dart
@@ -1,158 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:file_picker/file_picker.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:path/path.dart' as path;
-// import 'dart:io';
-// import 'Drawer.dart';
-// import 'BottomNavigation.dart';
-
-// class DocumentUpload extends StatefulWidget {
-//   const DocumentUpload({Key? key}) : super(key: key);
-
-//   @override
-//   _DocumentUploadState createState() => _DocumentUploadState();
-// }
-
-// class _DocumentUploadState extends State<DocumentUpload> {
-//   List<File> _uploadedFiles = [];
-
-//   Future<void> _pickDocument() async {
-//     final result = await FilePicker.platform.pickFiles(
-//       type: FileType.custom,
-//       allowedExtensions: ['pdf', 'doc', 'docx', 'txt'],
-//     );
-
-//     if (result != null) {
-//       final file = File(result.files.single.path!);
-//       final appDir = await getApplicationDocumentsDirectory();
-//       final savedDir = Directory('${appDir.path}/uploads');
-
-//       if (!await savedDir.exists()) {
-//         await savedDir.create(recursive: true);
-//       }
-
-//       final fileName = path.basename(file.path);
-//       final savedFile = await file.copy('${savedDir.path}/$fileName');
-
-//       setState(() {
-//         _uploadedFiles.add(savedFile);
-//       });
-//     }
-//   }
-
-//   void _deleteFile(int index) {
-//     setState(() {
-//       _uploadedFiles[index].deleteSync();
-//       _uploadedFiles.removeAt(index);
-//     });
-//   }
-
-//   Future<void> _listUploadedFiles() async {
-//     final appDir = await getApplicationDocumentsDirectory();
-//     final savedDir = Directory('${appDir.path}/uploads');
-//     final files = savedDir.listSync().map((item) => File(item.path)).toList();
-
-//     setState(() {
-//       _uploadedFiles = files;
-//     });
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _listUploadedFiles();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Document Upload'),
-//         backgroundColor: Colors.blue[300],
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: _uploadedFiles.isEmpty
-//             ? Center(
-//                 child: Text('No files uploaded.'),
-//               )
-//             : ListView.builder(
-//                 itemCount: _uploadedFiles.length,
-//                 itemBuilder: (context, index) {
-//                   return Dismissible(
-//                     key: Key(_uploadedFiles[index].path),
-//                     direction: DismissDirection.endToStart,
-//                     background: Container(
-//                       color: Colors.red,
-//                       padding: EdgeInsets.symmetric(horizontal: 20),
-//                       alignment: Alignment.centerLeft,
-//                       child: Icon(Icons.delete, color: Colors.white),
-//                     ),
-//                     onDismissed: (direction) {
-//                       _deleteFile(index);
-//                     },
-//                     child: ListTile(
-//                       title: Text(path.basename(_uploadedFiles[index].path)),
-//                       // subtitle: Text(_uploadedFiles[index].path),
-//                       onTap: () {
-//                         _openFile(_uploadedFiles[index]);
-//                       },
-//                     ),
-//                   );
-//                 },
-//               ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _pickDocument,
-//         tooltip: 'Upload Document',
-//         child: Icon(Icons.add),
-//         backgroundColor: Colors.blue[300],
-//       ),
-//       drawer: const DrawerMenu(),
-//       bottomNavigationBar: BottomNavBar(
-//         currentIndex: 0, // Set the current index for the Home screen
-//         onTap: (index) {
-//           switch (index) {
-//             case 0:
-//               Navigator.pushNamed(context, '/home');
-//               break;
-//             case 1:
-//               Navigator.pushNamed(context, '/search');
-//               break;
-//             case 2:
-//               Navigator.pushNamed(context, '/knowledge_base');
-//               break;
-//             default:
-//               break;
-//           }
-//         },
-//       ),
-//     );
-//   }
-
-//   Future<void> _openFile(File file) async {
-//     final content = await file.readAsString();
-
-//     showDialog(
-//       context: context,
-//       builder: (context) => AlertDialog(
-//         title: Text(path.basename(file.path)),
-//         content: SingleChildScrollView(
-//           child: Text(content),
-//         ),
-//         actions: <Widget>[
-//           TextButton(
-//             child: const Text('Close'),
-//             onPressed: () {
-//               Navigator.of(context).pop();
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:mindinsync/KnowledgeService.dart';
@@ -230,7 +75,7 @@ class _DocumentUploadState extends State<DocumentUpload> {
     knowledge.setKnowledge(content, userID);
   }
 
-void removeKnowledge(content) async {
+  void removeKnowledge(content) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? email = prefs.getString("userEmail");
     String userId = await db.getUserId(email!);
@@ -315,36 +160,56 @@ void removeKnowledge(content) async {
   }
 
   Future<void> _openFile(File file) async {
-    final content = await file.readAsString();
+    final fileExtension = path.extension(file.path).toLowerCase();
 
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(path.basename(file.path)),
-        content: SingleChildScrollView(
-          child: Text(content),
+    if (fileExtension == '.png' || fileExtension == '.jpg') {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(path.basename(file.path)),
+          content: Image.file(file),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Upload'),
-            onPressed: () {
-              insertKnowledge(content);
-            },
+      );
+    } else {
+      final content = await file.readAsString();
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(path.basename(file.path)),
+          content: SingleChildScrollView(
+            child: Text(content),
           ),
-          TextButton(
-            child: const Text('Remove'),
-            onPressed: () {
-              removeKnowledge(content);
-            },
-          ),
-          TextButton(
-            child: const Text('Close'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-    );
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Upload'),
+              onPressed: () {
+                insertKnowledge(content);
+              },
+            ),
+            TextButton(
+              child: const Text('Remove'),
+              onPressed: () {
+                removeKnowledge(content);
+              },
+            ),
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
